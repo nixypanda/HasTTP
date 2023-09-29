7c190e901b31109c093712607b57b5fd7fc65785
Sherub Thakur <sherub.thakur@gmail.com>
2023-09-29 21:20:57 +0530

    Setup to read cli option: --directory

--- app/Main.hs
+++ app/Main.hs
@@ -14,10 +14,27 @@ import Control.Monad.Except (
     runExceptT,
     throwError,
  )
+import Control.Monad.Reader (MonadReader, ReaderT, runReaderT)
 import Data.Bifunctor (first)
 import Data.ByteString.Char8 qualified as BSC
 import Data.Maybe (fromJust, fromMaybe)
 import Network.Simple.TCP (HostPreference (Host), Socket, recv, send, serve)
+import Options.Applicative (
+    Parser,
+    ParserInfo,
+    execParser,
+    fullDesc,
+    header,
+    help,
+    helper,
+    info,
+    long,
+    metavar,
+    optional,
+    progDesc,
+    strOption,
+    (<**>),
+ )
 import Parse (ParseError, parseHttpReq)
 import Types (
     HttpRequest (..),
@@ -35,8 +52,10 @@ bufferSize = 4096
 data HttpServerError = EmptyRequest | MalformedReq ParseError
     deriving (Show)
 
+newtype Env = Env {directory :: FilePath}
+
 newtype HttpServer a = HttpServer
-    { runHttpServer :: ExceptT HttpServerError IO a
+    { runHttpServer :: ReaderT Env (ExceptT HttpServerError IO) a
     }
     deriving
         ( Functor
@@ -44,6 +63,7 @@ newtype HttpServer a = HttpServer
         , Monad
         , MonadError HttpServerError
         , MonadIO
+        , MonadReader Env
         )
 
 handleClient :: Socket -> HttpServer ()
@@ -74,16 +94,40 @@ extractHeader :: HttpRequest -> HttpResponse
 extractHeader req =
     mkOkRes (fromMaybe "" $ getHeader req "User-Agent")
 
+newtype CliOpts = CliOpts
+    { maybeDir :: Maybe String
+    }
+
+environment :: Parser CliOpts
+environment = CliOpts <$> optional opt
+  where
+    opt =
+        strOption
+            ( long "directory"
+                <> metavar "TARGET"
+                <> help "Location of the directory to serve/store files to/from"
+            )
+
+cliOpts :: ParserInfo CliOpts
+cliOpts = info (environment <**> helper) desc
+  where
+    desc =
+        fullDesc
+            <> progDesc "A basic HTTP Server"
+            <> header "hs-http-server-clone - a barebones HTTP server written for fun"
+
 main :: IO ()
 main = do
     let host = "127.0.0.1"
         port = "4221"
 
     BSC.putStrLn $ "Listening on " <> BSC.pack host <> ":" <> BSC.pack port
+    CliOpts{..} <- execParser cliOpts
+    let env = Env $ fromMaybe "." maybeDir
 
     serve (Host host) port $ \(sock, addr) -> do
         BSC.putStrLn $ "Accepted connection from " <> BSC.pack (show addr) <> "."
-        errOrRes <- runExceptT (runHttpServer $ handleClient sock)
+        errOrRes <- runExceptT $ runReaderT (runHttpServer $ handleClient sock) env
         case errOrRes of
             Left err -> BSC.putStrLn $ "Error: " <> BSC.pack (show err)
             Right _ -> pure ()
