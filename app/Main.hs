7a928cce72ea8b33eb649948c54bb4c0333f4e1a
Sherub Thakur <sherub.thakur@gmail.com>
2023-09-29 20:45:47 +0530

    bind to a port

--- app/Main.hs
+++ app/Main.hs
@@ -1,20 +1,17 @@
+{-# LANGUAGE ImportQualifiedPost #-}
 {-# LANGUAGE OverloadedStrings #-}
 
 module Main (main) where
 
-import qualified Data.ByteString.Lazy.Char8 as BLC
-import Network.Simple.TCP
+import Data.ByteString.Lazy.Char8 qualified as BLC
+import Network.Simple.TCP (HostPreference (Host), serve)
 
 main :: IO ()
 main = do
-    -- You can use print statements as follows for debugging, they'll be visible when running tests.
-    BLC.putStrLn "Logs from your program will appear here"
+    let host = "127.0.0.1"
+        port = "4221"
 
-    -- Uncomment this block to pass first stage
-    -- let host = "127.0.0.1"
-    --     port = "4221"
-    --
-    -- BLC.putStrLn $ "Listening on " <> BLC.pack host <> ":" <> BLC.pack port
-    --
-    -- serve (Host host) port $ \(serverSocket, serverAddr) ->
-    --     BLC.putStrLn $ "Accepted connection from " <> BLC.pack (show serverAddr) <> "."
+    BLC.putStrLn $ "Listening on " <> BLC.pack host <> ":" <> BLC.pack port
+
+    serve (Host host) port $ \(sock, addr) ->
+        BLC.putStrLn $ "Accepted connection from " <> BLC.pack (show addr) <> "."
